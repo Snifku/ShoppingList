a2a006b336bebf74d584e63496fc6f3df1beb7b6
Opilane TTHK <opilane@tthk.ee>
2020-12-02 15:01:00 +0200

    first commit

---
+++ DirectoriesAndFiles/WriteFilesAndDirectories/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WriteFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFilePaths();
+        }
+            public static void WriteFilePaths()
+        {
+            string filePath = @"C:\Users\opilanesamples\files.txt";
+            string directoryPath = @"C:\Users\opilanesamples";
+
+            string[] files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
+
+            //save to file
+            File.WriteAllLines(filePath, files);
+            File.WriteAllLines(fileTxt, allFiles);
+            Console.WriteLine("");
+        }
+        
+    }
+}
---
+++ GettersAndSetters/GettersAndSetters/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace GettersAndSetters
+{
+    class Program
+    {
+        class Person
+        {
+            string name;
+            int age;
+            string gender;
+            string idCode;
+
+            public Person(string _name, string _gender, int _age, string _idCode)
+            {
+                Name = _name;
+                Gender = _gender;
+                age = _age;
+                IdCode = _idCode;
+            }
+            public void ChangeName(string newName)
+            {
+                name = newName;
+            }
+            public string Name
+            {
+                get { return name; }
+                set
+                {
+                    name = value;
+                }
+            }
+
+            public string Gender
+            {
+                get { return gender; }
+                set
+                {
+                    if (value == "male" || value == "female")
+                    {
+                        gender = value;
+                    }
+                    else
+                    {
+                        gender = "undefined unicorn";
+                    }
+                }
+            }
+            public string IdCode
+            {
+                get { return idCode; }
+                set
+                {
+                    if (value.Length == 11)
+                    {
+                        idCode = value;
+                    }
+                    else
+                    {
+                        idCode = "undefined";
+                    }
+                }
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Person newPerson = new Person("Harry Potter", "gmail", 35, "1234");
+            //Console.WriteLine(newPerson.Age);
+            Console.WriteLine(newPerson.Name);
+            Console.WriteLine(newPerson.Gender);
+            //newPerson.Name = "Harry Potter";
+            //newPerson.Gender = "male";
+            //newPerson.Age = 21;
+            //Console.WriteLine(newPerson.Age);
+           // Console.WriteLine(newPerson.Gender);
+            Console.WriteLine(newPerson.Name);
+            newPerson.Name = "Harry LSD";
+            newPerson.ChangeName("Harry LSD");
+        }
+    }
+}
